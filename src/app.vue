c3a2c8cf833bd38e69247d21bf849b485d529c7b
fu731033719 <15706804793@163.com>
2018-07-23 15:31:41 +0800

    基于webpack3 打包测试

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="text">{{text}}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: 'abc',
+    }
+  }
+}
+</script>
+<style>
+#text {
+  color: red
+}
+</style>
