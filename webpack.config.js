47b73250a2f9ab1c844a0003350dafc08abf2fec
fu731033719 <15706804793@163.com>
2018-07-24 14:50:33 +0800

    webpack  dev开发模式配置  热更新模式配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
-
-module.exports = {
+const webpack = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+const config = {
+  target: 'web',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'bundle.js',
@@ -40,5 +43,35 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new HTMLPlugin()
+  ]
 }
+
+if (isDev) {
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: '8000',
+    host: '0.0.0.0',
+    overlay: {
+      errors: true,
+    },
+    // open: true,
+    // historyFallback: {
+      
+    // },
+    hot: true
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()  
+  )
+}
+
+module.exports = config
