38255bc8dc129963c522f9d6473ce73c34580321
fuchenwei <fuchenwei@citytogo.com>
2018-07-27 12:10:55 +0800

    CSS modules 模块化编译类名尝试 （相关代码已注释留存）

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -2,6 +2,9 @@ module.exports = (isDev) => {
   return {
     preserveWhitepace: true,
     extractCSS: !isDev,
-    cssModules: {}
+    cssModules: {
+      localIdentName: '[path]-[name]-[hash:base64:5]',
+      camelCase: true
+    },
   }
 }
--- client/layout/footer.jsx
+++ client/layout/footer.jsx
@@ -1,5 +1,5 @@
+// import className from '../assets/styles/footer.styl'
 import '../assets/styles/footer.styl'
-
 export default {
   data() {
     return {
@@ -8,6 +8,7 @@ export default {
   },
   render() {
     return (
+      // <div id={className.footer}>
       <div id="footer">
         <span>Written by {this.author}</span>
       </div>
