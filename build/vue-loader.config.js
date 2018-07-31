d24baf6a0929c5ebaa0f04059b6f895c0c51ad7a
fuchenwei <fuchenwei@citytogo.com>
2018-07-31 17:03:52 +0800

    解决替换mini-css-extract-plugin后的生产环境打包失败问题 vue-loader中extractCSS配置对extract-text-webpack-plugin具有依赖性 暂未找到mini-css-extract-plugin + vue-loader的对.vue文件中的样式单独提取打包的方法

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,10 +1,9 @@
 module.exports = (isDev) => {
   return {
     preserveWhitepace: true,
-    extractCSS: !isDev,
     cssModules: {
       localIdentName: '[path]-[name]-[hash:base64:5]',
       camelCase: true
-    },
+    }
   }
 }
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
-// const ExtractPlugin = require('extract-text-webpack-plugin')
 const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
@@ -18,7 +17,7 @@ const devServer = {
   port: 8000,
   host: '0.0.0.0',
   overlay: {
-    errors: true,
+    errors: true
   },
   hot: true
 }
@@ -33,17 +32,10 @@ if (isDev) {
           use: [
             'vue-style-loader',
             'css-loader',
-            // {
-            //   loader: 'css-loader',
-            //   options: {
-            //     module: true,
-            //     localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
-            //   }
-            // },使用CSSmodule编译模式
             {
               loader: 'postcss-loader',
               options: {
-                sourceMap: true,
+                sourceMap: true
               }
             },
             'stylus-loader'
@@ -61,7 +53,6 @@ if (isDev) {
   config = merge(baseConfig, {
     entry: {
       app: path.join(__dirname, '../client/index.js')
-      // vendor: ['vue']
     },
     output: {
       filename: '[name].[chunkhash:8].js'
@@ -72,29 +63,15 @@ if (isDev) {
           test: /\.styl/,
           use: [
             MiniCssExtractPlugin.loader,
+            'css-loader',
             {
               loader: 'postcss-loader',
               options: {
-                sourceMap: true,
+                sourceMap: true
               }
             },
-            'css-loader',
             'stylus-loader'
           ]
-          // use: ExtractPlugin.extract({
-          //   fallback: 'style-loader',
-          //   use: [
-          //     'css-loader',
-          //     {
-          //       loader: 'postcss-loader',
-          //       options: {
-          //         sourceMap: true,
-          //       }
-          //     },
-          //     'stylus-loader'
-          //   ]
-          // })
-
         }
       ]
     },
@@ -105,17 +82,9 @@ if (isDev) {
       runtimeChunk: true
     },
     plugins: defaultPluins.concat([
-      // new ExtractPlugin('styles.[contentHash:8].css')
       new MiniCssExtractPlugin({
-        filename: 'styles.[name].[contentHash:8].css',
-        chunkFilename: 'styles.[id].[contentHash:8].css'
+        filename: 'styles.[name].[contentHash:8].css'
       })
-      // new webpack.optimize.CommonsChunkPlugin({
-      //   name: 'vendor'
-      // }),
-      // new webpack.optimize.CommonsChunkPlugin({
-      //   name: 'runtime'
-      // })
     ])
   })
 }
